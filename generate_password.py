774efa5e13cdfe73df7c5a56f974d19b23307408
oskaragzigitov <oskar.agzigitov@yanex.ru>
2025-01-29 14:25:21 +0300

    Initial commit

---
+++ generate_password.py
@@ -0,0 +1,13 @@
+import string
+import random
+
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(random.choice(characters) for _ in range(length))
+    return password
+
+
+password_length = int(8)
+generated_password = generate_password(password_length)
+print("Ваш сгенерированный пароль:", generated_password)
